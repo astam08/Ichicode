f2d48c5e1e308f891b51a747c2f5ca18edc1343e
astam08 <49343371+astam08@users.noreply.github.com>
2019-04-07 16:59:38 +0700

    Update and rename config.ex.js to config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.master = 'YOUR_TOKEN_ID_HERE';
-exports.token = 'YOUR_BOT_TOKEN_HERE';
-exports.ytkey = 'YOUR_YOUTUBE_KEY_HERE';
+exports.token = 'NTUxMzM4NTI2OTUyOTE0OTUz.XKnGGA.VlXKoe-Kplu-3oQJ79-gY9k4K9Q';
+exports.ytkey = 'AIzaSyDtnhiJiY9vZk-5rGwSDnGY8LyiaNfQRWs';
 
 /* IDs for bot's test server. This allows users from guilds to report and request features. */
 exports.masterServer = 'YOUR_MASTER_BOT_SERVER_ID_HERE';
