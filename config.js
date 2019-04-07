2f489542b0993299d4585efdb7961f16c79aa8e2
astam08 <49343371+astam08@users.noreply.github.com>
2019-04-07 17:24:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ exports.ytkey = 'AIzaSyDtnhiJiY9vZk-5rGwSDnGY8LyiaNfQRWs';
 /* IDs for bot's test server. This allows users from guilds to report and request features. */
 exports.masterServer = 'YOUR_MASTER_BOT_SERVER_ID_HERE';
 exports.reportChannel = 'YOUR_MASTER_BOT_SERVER_REPORT_CHANNEL_ID_HERE';
-exports.requestChannel = 'YOUR_MASTER_BOT_SERVER_REQUEST_CHANNEL_ID_HERE';
+exports.requestChannel = '563415077831835651';
 exports.feedbackChannel = 'YOUR_MASTER_BOT_SERVER_FEEDBACK_CHANNEL_ID_HERE';
 exports.dmChannel = 'YOUR_MASTER_BOT_SERVER_DM_CHANNEL_ID_HERE';
